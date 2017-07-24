77ee8e87b99339bcdfd1d89f6a454cbadd26e7f6
Berk Alp Yakici <berkalp.y@gmail.com>
2017-07-24 19:40:07 +0300

    Applying to a Conference

--- panel/conference/conference.php
+++ panel/conference/conference.php
@@ -35,6 +35,31 @@
       </div>
       <? } ?>
 
+      <? if($localmaster["independent"]) { ?>
+      <? if(!IS_ADVISOR) { ?>
+      <div class="alert alert-success text-left">
+        <? if($usrapplied) { ?>
+        <button type="button" class="btn btn-success pull-right" data-toggle="modal" data-target="#reviewApp"> Review Application</button>
+        You have applied this conference.<br><small>Please review your application to meet admission criteria and deadlines.</small>
+        <? } else { ?>
+        <form id="applyConf" action="conference.php" method="post" accept-charset="UTF-8">
+          <input type="hidden" name="type" value="applyConf" />
+          <input type="hidden" name="conference" value="<?= $localid ?>" />
+          <button type="submit" id="applyConfButton" class="btn btn-success pull-right">Apply Now</button>
+        </form>
+        <strong><?= $numapplied ?></strong> member(s) applied this conference.<br><small>Apply to receive recommendation letter from your MUN advisor.</small>
+        <? } ?>
+      </div>
+      <? } ?>
+
+      <div class="alert alert-info text-left">
+        Internal Deadlines
+        <br><small>Deadline for Internal Application: 23.05.2017</small>
+        <br><small>Deadline for Document Signatures: 23.05.2017</small>
+        <br><small>Deadline for Recommendation: 23.05.2017</small>
+      </div>
+      <? } ?>
+
       <div class="jumbotron">
         <h2 style="margin-top: 15px; margin-bottom: 15px;"><?= $localmaster["name"] ?></h2>
         <h4>Hosted by <?= $localmaster["host"] ?> in <?= $localmaster["city"] ?>, <?= $localmaster["country"] ?></h4>
@@ -55,6 +80,49 @@
           </div>
         </div>
       </div>
+
+      <div class="modal fade" id="reviewApp" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title">Review Your Application</h4>
+            </div>
+            <div class="modal-body">
+              <ul class="list-group">
+                <li class="list-group-item">
+                  <span class="badge"><i class="fa fa-check" aria-hidden="true"></i> Verified</span>
+                  Internal Application
+                  <br><small>Application Received</small>
+                </li>
+
+                <li class="list-group-item">
+                  <span class="badge"><i class="fa fa-times" aria-hidden="true"></i> Pending</span>
+                  Legal Documents
+                  <br><small>Intependent Conference Permission Form is Pending</small>
+                  <br><small><a href="">Download the Form</a></small>
+                </li>
+
+                <li class="list-group-item">
+                  <span class="badge"><i class="fa fa-times" aria-hidden="true"></i> Pending</span>
+                  Optional: Recommendation Letter
+                  <br><small>If this conference requires a recommendation letter, select an advisor.</small>
+                  <br>
+                  <select class="form-control">
+                    <option>N/A</option>
+                    <option>Candan Yavuz</option>
+                  </select>
+                </li>
+              </ul>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Remove Application</button>
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <? } ?>
 
       <? include(PATH_PRIVATE."page/footer.php"); ?>
@@ -72,39 +140,9 @@
 <script>
 $(document).ready(function() {
     // bind 'myForm' and provide a simple callback function
-    $('#formStuInf').ajaxForm(function(data) {
+    $('#applyConf').ajaxForm(function(data) {
         if(data == "ok") {
           location.reload();
-        }
-        else if(data == "empty") {
-          swal("Something went wrong!", "Please fill the required fields!", "error");
-        }
-        else if(data == "email") {
-          swal("Something went wrong!", "The email address you've provided is already in use.", "error");
-        }
-        else {
-          swal("Something went wrong!", "Please try again later.", "error");
-        }
-    });
-
-    $('#formPrntInf').ajaxForm(function(data) {
-        if(data == "ok") {
-          location.reload();
-        }
-        else if(data == "empty") {
-          swal("Something went wrong!", "Please fill the required fields!", "error");
-        }
-        else {
-          swal("Something went wrong!", "Please try again later.", "error");
-        }
-    });
-
-    $('#formAdvInf').ajaxForm(function(data) {
-        if(data == "ok") {
-          location.reload();
-        }
-        else if(data == "empty") {
-          swal("Something went wrong!", "Please fill the required fields!", "error");
         }
         else {
           swal("Something went wrong!", "Please try again later.", "error");
